b7ca1099536e55593c74a4cfb40c6978d6675bf7
Habib-bhai <habibahmed918131@gmail.com>
2024-07-24 19:58:26 +0500

    day11 done

--- day11/main.js
+++ day11/main.js
@@ -48,18 +48,55 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //     console.log(result);
 // }
 // prom()
-async function promRejection() {
-    try {
-        let result = await new Promise((res, rej) => {
-            setTimeout(() => {
-                rej("Unfortunately! Promise got rejected");
-            }, 2000);
-        });
-    }
-    catch (val) {
-        if (typeof val === "string") {
-            throw new Error(val);
-        }
-    }
-}
-promRejection();
+// TASK 5
+// async function promRejection() {
+//     try {
+//         let result = await new Promise((res,rej) => {
+//             setTimeout(() => {
+//                 rej("Unfortunately! Promise got rejected")
+//             }, 2000);
+//         })
+//     }
+//     catch(val: unknown){
+//         if(typeof val=== "string"){
+//             throw new Error(val)
+//         }
+//     }
+// }
+// promRejection()
+//  Activity.NO.4: Fetching DATA from an API.
+// TASK 6
+let url = "https://api.openweathermap.org/data/2.5/weather?q=Karachi&appid=87b0b3db79197977b32e6b0c8819181e";
+// fetch(url).then(res => {
+//     return res.json()
+// }).then(jsonResponse => {
+//     console.log(jsonResponse);
+// })
+// TASK 7
+// async function dataFetching(){
+//     let response = await fetch(url)
+//     let finalJsonResponse = await response.json()
+//     console.log(finalJsonResponse);
+// }
+// dataFetching()
+// Activity.NO.5: Concurrent Promises
+// TASK 8
+let prom1 = new Promise((res, rej) => {
+    // console.log("Hello world ");
+    setTimeout(() => {
+        res("Inside promise 1");
+    }, 3000);
+});
+let prom2 = new Promise((res, rej) => {
+    setTimeout(() => {
+        res("Inside promise 2");
+    }, 2000);
+});
+let prom3 = new Promise((res, rej) => {
+    setTimeout(() => {
+        res("Inside promise 3");
+    }, 5000);
+});
+// Promise.all([prom1,prom2,prom3]).then(val => console.log(val))
+// TASK 9
+Promise.race([prom1, prom2, prom3]).then(val => console.log(val));

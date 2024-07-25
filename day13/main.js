b23bc5a0900e265f147335c66e7cd877d7214359
Habib-bhai <habibahmed918131@gmail.com>
2024-07-25 13:44:58 +0500

    day13 done

---
+++ day13/main.js
@@ -0,0 +1,32 @@
+// MODULES
+// Activity.NO.1: Creating and Exporting Modules
+// TASK 1
+// import {sum} from "./module"
+// sum(10, 20)
+// TASK 2
+// import { user } from "./module"
+// console.log(user.name);
+// console.log(user.greet());
+// Activity.NO.2: Named & Default Exports
+// TASK 3
+// import {add,subtract,multiplication,division} from "./module"
+// add()
+// subtract()
+// multiplication()
+// division()
+// TASK 4
+// import square from "./module"
+// square(12)
+// Activity.NO.3: Importing Entire modules.
+// TASK 5
+// import * as obj from "./module"
+// obj.default(2)
+//  Activity.NO.4: Using Third Party Modules.
+// TASK 6
+// Using third party modules "lodash"
+// import _ from "lodash"
+// let str = "habib"
+// console.log(_.capitalize(str));
+import axios from "axios";
+axios.get("https://jsonplaceholder.typicode.com/posts/1")
+    .then(res => console.log(res));
---
+++ day13/module.js
@@ -0,0 +1,26 @@
+// export function sum(a,b) {
+//     console.log(a + b);
+export {};
+// }
+// export let user = {
+//     name: "Habib",
+//     greet(){
+//         return `Hello ${this.name}!`;
+//     }
+// }
+// function add() {
+//     console.log(10 + 10);
+// }
+// function subtract(){
+//     console.log(20 -10);
+// }
+// function multiplication(){
+//     console.log(10 * 10);
+// }
+// function division(){
+//     console.log(20 /10);
+// }
+// export {add,subtract,multiplication,division}
+// export default function square(num: number){
+//     console.log(num**2);
+// }
